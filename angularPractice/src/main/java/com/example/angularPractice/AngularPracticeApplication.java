56a400517407f2a6586bdbbe16d23e8b885d5248
peterlokey <peter.lokey1@gmail.com>
2020-02-12 15:39:24 -0600

    Initial Commit

---
+++ angularPractice/src/main/java/com/example/angularPractice/AngularPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.angularPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularPracticeApplication.class, args);
+	}
+
+}
---
+++ angularPractice/src/test/java/com/example/angularPractice/AngularPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.angularPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
